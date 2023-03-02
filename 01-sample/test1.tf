4fc56bc2586b9801a03e65d4cd5f08743d890443
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-03-02 11:43:08 +0530

    aws instance using terraform

---
+++ 01-sample/test1.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-068a20157a2378db8"]
+  tags = {
+    Name = "test"
+  }
+}
