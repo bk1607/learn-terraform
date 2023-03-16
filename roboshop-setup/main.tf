9cd1a72f4635bba653432306ae635b7d283fdaf6
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-03-16 22:08:11 +0530

    state file

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-statefile-12"
+    key    = "statefiles/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_instance" "ec2" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t3.micro"
