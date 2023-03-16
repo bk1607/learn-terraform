f8b83bf82b69ca95f544b8f1312887aa2d9d1ec2
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-03-16 12:01:11 +0530

    frontend

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "ec2" {
   ami           = "ami-0089b8e98cd95257d"
   instance_type = "t3.micro"
-  instance_interruption_behaviour = "stop"
+  instance_interruption_behavior = "stop"
   vpc_security_group_ids = ["sg-068a20157a2378db8"]
 
   tags = {
