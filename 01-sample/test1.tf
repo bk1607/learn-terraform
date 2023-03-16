1be40db62e7abaee45b3e955c2703977cfbb1f8c
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-03-16 21:58:47 +0530

    ami

--- 01-sample/test1.tf
+++ 01-sample/test1.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0a017d8ceb274537d"
+  ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-068a20157a2378db8"]
   tags = {
