bbba8e82ab27bbdd25a691b1717fe4b1dd3b5abb
bk1607 <baluanumala16@gmail.com>
2023-09-12 09:58:17 +0530

    data block for ami

--- 01-sample/test1.tf
+++ 01-sample/test1.tf
@@ -1,8 +1,14 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0089b8e98cd95257d"
+  ami = data.aws_ami.ami_id.image_id
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-068a20157a2378db8"]
   tags = {
     Name = "test"
   }
+}
+
+data "aws_ami" "ami_id" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
 }
