92745e46e33cb9fc8bf242dc4f48f298e439004e
bk1607 <baluanumala16@gmail.com>
2023-09-13 09:45:02 +0530

    modules in terraform

---
+++ 06-modules/main.tf
@@ -0,0 +1,9 @@
+module "ec2" {
+  source = "/ec2"
+  component = var.instances.component
+  instance_type = var.instances.instance_type
+}
+variable "instances" {
+  component = "frontend"
+  instance_type = "t3.micro"
+}
