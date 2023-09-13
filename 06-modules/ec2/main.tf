6fd0f47a7105503f4247f513353f09da566859c8
bk1607 <baluanumala16@gmail.com>
2023-09-13 09:59:13 +0530

    modules in terraform

--- 06-modules/ec2/main.tf
+++ 06-modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami = "ami-0089b8e98cd95257d"
+  ami = "ami-03265a0778a880afb"
   instance_type = var.instance_type
   vpc_security_group_ids = ["sg-068a20157a2378db8"]
   tags = {
