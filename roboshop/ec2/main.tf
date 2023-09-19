efa914024a0bd9a60c23d3a77b7fce80f533c9d7
bk1607 <baluanumala16@gmail.com>
2023-09-19 11:58:50 +0530

    ec2 sg

--- roboshop/ec2/main.tf
+++ roboshop/ec2/main.tf
@@ -11,7 +11,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_spot_instance_request" "ec2" {
   ami = data.aws_ami.ami_id.id
   instance_type = var.instance_type
-  security_groups = [var.sg_id]
+  vpc_security_group_ids = [var.sg_id]
   spot_type = "persistent"
   instance_interruption_behavior = "stop"
   tags = {
