e10bd8a58cc40d48ada139fa96e74d6eafb31462
bk1607 <baluanumala16@gmail.com>
2023-09-19 11:00:14 +0530

    ec2 module

--- roboshop/ec2/main.tf
+++ roboshop/ec2/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bk"
-    key    = "ec2/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 data "aws_ami" "ami_id" {
   most_recent      = true
   name_regex       = "devops-practice-ansible"
