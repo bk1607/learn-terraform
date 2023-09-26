9389b41936e00a1d1436d3b43ce54fe151810100
bk1607 <baluanumala16@gmail.com>
2023-09-26 17:30:35 +0530

    iam role for ssm parameters

---
+++ roboshop/iam_user/main.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role" "example" {
+  name = ssm_parameters_role
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:Describe*",
+                "ssm:Get*",
+                "ssm:List*"
+            ],
+            "Resource": "*"
+        }
+    ]
+})
+}
