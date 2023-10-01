d0954ed4f5cdc5d42b69254a89902ebc3a4a728d
bk1607 <baluanumala16@gmail.com>
2023-10-01 10:30:49 +0530

    iam policy for ssm parameters

--- roboshop/iam_user/main.tf
+++ roboshop/iam_user/main.tf
@@ -1,7 +1,49 @@
-resource "aws_iam_policy" "example" {
-  name = "example"
-  policy = jsonencode({
+# to create iam policy
+#resource "aws_iam_policy" "example" {
+#  name = "example"
+#  policy = jsonencode({
+#
+#    "Version": "2012-10-17",
+#    "Statement": [
+#        {
+#            "Effect": "Allow",
+#            "Action": [
+#                "ssm:Describe*",
+#                "ssm:Get*",
+#                "ssm:List*"
+#            ],
+#            "Resource": "*"
+#        }
+#    ]
+#})
+#
+#}
 
+# to create a role
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# to attach the example policy to a role
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+  policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
         {
@@ -12,8 +54,8 @@ resource "aws_iam_policy" "example" {
                 "ssm:List*"
             ],
             "Resource": "*"
-        }
+      },
     ]
-})
-
+  })
 }
+
