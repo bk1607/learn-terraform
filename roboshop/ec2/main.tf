b1a655023d4f1f5acba6bfa57ac76f58ea8d9213
bk1607 <baluanumala16@gmail.com>
2023-10-03 09:44:26 +0530

    re structuring main.tf file

--- roboshop/ec2/main.tf
+++ roboshop/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2" {
   ami = data.aws_ami.ami_id.id
   instance_type = var.instance_type
   vpc_security_group_ids = [var.sg_id]
-  iam_instance_profile = aws_iam_instance_profile.test_profile.name
+  iam_instance_profile = "${var.component}-${var.env}-role"
   tags = {
     Name = var.instance_name
   }
