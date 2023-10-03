bcd6337fc3576e4adb32a11472029900b74cbaa8
bk1607 <baluanumala16@gmail.com>
2023-10-03 09:02:17 +0530

    re structuring main.tf file

---
+++ roboshop/ec2/data.tf
@@ -0,0 +1,13 @@
+#data block to get ami_id of specified ami
+data "aws_ami" "ami_id" {
+  most_recent      = true
+  name_regex       = "devops-practice-ansible"
+  owners           = [data.aws_caller_identity.current.account_id]
+}
+
+#data block to get owner id to use in ami block
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_instance_profile" "test" {
+  name = "test_profile"
+}
--- roboshop/ec2/main.tf
+++ roboshop/ec2/main.tf
@@ -1,17 +1,3 @@
-#data block to get ami_id of specified ami
-data "aws_ami" "ami_id" {
-  most_recent      = true
-  name_regex       = "devops-practice-ansible"
-  owners           = [data.aws_caller_identity.current.account_id]
-}
-
-#data block to get owner id to use in ami block
-data "aws_caller_identity" "current" {}
-
-data "aws_iam_instance_profile" "test" {
-  name = "test_profile"
-}
-
 #create ec2 instance
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ami_id.id
@@ -35,53 +21,3 @@ output "pvt_ip" {
   value = aws_instance.ec2.private_ip
   depends_on = [aws_instance.ec2]
 }
-#resource "aws_iam_policy" "example" {
-#  name = "example"
-#  policy = jsonencode({
-#
-#    "Version": "2012-10-17",
-#    "Statement": [
-#        {
-#            "Effect": "Allow",
-#            "Action": [
-#                "ssm:Describe*",
-#                "ssm:Get*",
-#                "ssm:List*"
-#            ],
-#            "Resource": "*"
-#        }
-#    ]
-#})
-#
-#}
-#
-## to create a role
-#
-#resource "aws_iam_role" "test_role" {
-#  name = "test_role"
-#
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Principal = {
-#          Service = "ec2.amazonaws.com"
-#        }
-#      },
-#    ]
-#  })
-#}
-#
-## to attach the example policy to a role
-#resource "aws_iam_role_policy_attachment" "test-attach" {
-#  role       = aws_iam_role.test_role.name
-#  policy_arn = aws_iam_policy.example.arn
-#}
-#
-##we need to create an instance profile for our role to attach
-#resource "aws_iam_instance_profile" "test_profile" {
-#  name = "test_profile"
-#  role = aws_iam_role.test_role.name
-#}
