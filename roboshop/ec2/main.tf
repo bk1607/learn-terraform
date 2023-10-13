9e90a6a238d50206a67b38114694043b5ef35d29
bk1607 <baluanumala16@gmail.com>
2023-10-13 10:38:52 +0530

    commenting remote-exec

--- roboshop/ec2/main.tf
+++ roboshop/ec2/main.tf
@@ -104,17 +104,17 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-resource "null_resource" "commands" {
-  depends_on = [aws_route53_record.www]
-  provisioner "remote-exec" {
-    connection {
-      host = aws_instance.ec2.public_ip
-      user = "centos"
-      password = "DevOps321"
-    }
-    inline = [
-     "ansible-pull -i localhost, -U https://github.com/bk1607/ansible-roboshop roboshop.yml -e role_name=${var.component} -e env=${var.env}"
-    ]
-
-  }
-}
+#resource "null_resource" "commands" {
+#  depends_on = [aws_route53_record.www]
+#  provisioner "remote-exec" {
+#    connection {
+#      host = aws_instance.ec2.public_ip
+#      user = "centos"
+#      password = "DevOps321"
+#    }
+#    inline = [
+#     "ansible-pull -i localhost, -U https://github.com/bk1607/ansible-roboshop roboshop.yml -e role_name=${var.component} -e env=${var.env}"
+#    ]
+#
+#  }
+#}
